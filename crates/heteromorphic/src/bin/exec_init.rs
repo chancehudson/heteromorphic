2c4894f69b457fe1c0ef8f9f867610e0944e2615
chance <jchancehud@gmail.com>
2025-08-07 21:20:50 -0500

    refactor: use ZKInitProgram struct directly

--- crates/heteromorphic/src/bin/exec_init.rs
+++ crates/heteromorphic/src/bin/exec_init.rs
@@ -2,18 +2,17 @@ use heteromorphic_zk::prelude::*;
 use zkpo::prelude::*;
 
 fn main() -> anyhow::Result<()> {
-    let program = ZKInitProgram;
     let key = [0u8; 32];
     let mem_len = 32;
     let nonce = [0u8; 12];
 
     let input = (key, mem_len, nonce);
     println!("Initializing {mem_len} bytes in chacha in zk...");
-    let exe = program.execute(&bincode::serialize(&input)?, None)?;
+    let exe = ZKInitProgram.execute(&bincode::serialize(&input)?, None)?;
     println!("Executed.");
     println!("Verifying....");
 
-    let output = program.agent().verify(&*exe);
+    let output = ZKInitProgram.agent().verify(&*exe);
     println!("verified init data: {output:?}");
     Ok(())
 }
